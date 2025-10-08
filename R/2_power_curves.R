ff513a243395c317c0a03fda703c1ed790de9b8c
Jack Mulqueeney <jmulqueeney@uchicago.edu>
2025-10-08 13:17:08 -0500

    add further plot annotations for clarity, change to only using negative treatment effects

--- R/2_power_curves.R
+++ R/2_power_curves.R
@@ -21,8 +21,8 @@ set.seed(1235901350)
 # 0.1 simulation parameters
 J <- 30 # No. agencies
 t <- 4 * 12 # No. years
-rho_vec <- seq(-0.03, 0.03, 0.0075) # Treatment effect sizes
-M <- 150 # Simulation reps
+rho_vec <- seq(-0.03, 0, 0.003) # Treatment effect sizes
+M <- 200 # Simulation reps
 
 # Average number of probationers over time for each agency
 Nj <- extraDistr::rdunif(n = J, 
@@ -57,8 +57,6 @@ agency_data <- agency_data %>%
 
 # Create time-varying agency size
 agency_data$Njt <- agency_data$Nj + extraDistr::rdunif(n = J * t, min = -15, max = 15)
-
-
 
 # Detect available cores
 n_cores <- parallel::detectCores() - 1
@@ -99,7 +97,7 @@ ggplot(data = power_res, aes(x = delta, y = power)) +
   geom_point() + 
   geom_smooth(se = FALSE, span = 0.5, colour = "grey80") +
   theme_minimal() +
-  labs(title = "Power Curves", 
+  labs(title = "Power Curve for TWFE", 
        x = expression(delta), 
        y = "Power",
        caption = str_wrap("Points represent frequency with which a two-tailed 5% t-test rejects the false H0: delta = 0. Smoothed line generated using LOESS."), 
@@ -113,12 +111,24 @@ ggplot(data = power_res, aes(x = delta, y = power)) +
   geom_hline(yintercept = conf_lev, 
              linetype = "dashed", 
              colour = "firebrick") +
+  geom_hline(yintercept = 0.8, 
+             linetype = "dashed", 
+             colour = "steelblue") +
   annotate(
     "text",
-    x = max(power_res$delta),          # right side of plot
-    y = conf_lev + 0.05,               # slightly above line
+    x = max(power_res$delta),       
+    y = conf_lev + 0.05,            
     label = bquote(alpha == .(conf_lev)),
     colour = "firebrick",
-    hjust = 1,                         # right-justified
+    hjust = 1,                        
+    size = 3.5
+  ) +
+  annotate(
+    "text",
+    x = max(power_res$delta),       
+    y = 0.8 - 0.075,                  
+    label = "80% power",
+    colour = "steelblue",
+    hjust = 1,
     size = 3.5
   )
