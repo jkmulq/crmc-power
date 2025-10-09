b5b120b164545ee4b22130ba00e67123e0bcd20d
Jack Mulqueeney <jmulqueeney@uchicago.edu>
2025-10-09 17:33:42 -0500

    Fully standalone bias script, and redraw treatment periods every time.

--- R/3_estimator_bias.R
+++ R/3_estimator_bias.R
@@ -1,15 +1,86 @@
 # QUick sim
+rm(list = ls())
+set.seed(83141984)
+
+# Directory
+setwd("~/Documents/Dev/crmc-power/R")
+
+# Packages
+library(tidyverse)
+library(extraDistr)
+library(ggfixest)
+source("0_functions.R") # Source functions
+
+# Seed
+set.seed(1235901350)
+
+## 0 Preliminaries
+# 0.1 simulation parameters
+J <- 30 # No. agencies
+t <- 12 * 4 # No. years
+rho_vec <- seq(-0.025, 0, 0.002) # Treatment effect sizes
+rho <- -0.05
+M <- 100 # Simulation reps
+
+# Average number of probationers over time for each agency
+Njmin <- 20
+Njmax <- 200
+Nj <- extraDistr::rdunif(n = J,
+                         min = Njmin, # Minimum number
+                         max = Njmax) # Maximum number
+
+# True recidivism rate across agencies
+pj <- runif(n = J, 
+            min = 0.2, # Minimum rate. Must be >= min treatment effect
+            max = 0.5) # Maximum rate. 1 - this > max treatment
+
+# 0.2 Simulation parameter summary
+sim_params <- data.frame("j" = 1:J,
+                         "Nj" = Nj,
+                         "pj_pre" = pj)
+
+# 0.2 Setup storage matrix for data
+agency_data <- tibble(agency = rep(0, J * t))
+agency_data$agency <- sort(rep(1:J, t)) # Setup agency index
+agency_data$period <- rep(1:t, J) # Setup time index
+
+# Join on simulation params
+agency_data <- left_join(agency_data, sim_params, by = join_by("agency" == "j"))
+
+# Create time-varying agency size
+agency_data$Njt <- agency_data$Nj + extraDistr::rdunif(n = J * t, min = -15, max = 15)
+
+# Detect available cores
+n_cores <- parallel::detectCores() - 1
+cat("Using", n_cores, "cores\n")
+
 store <- vector("list", length = M)
 
-set.seed(83141984)
+# Create dummy treatment variable in agency_data. 
+# Loop overwrites this with actual treatment period
+
 
 for (i in 1:M){
   
+  # Treatment period for each agency (add 12 to account for first year)
+  tj <- sample(13:t, size = J, replace = FALSE)
+  
+  # Join treatment dates onto agency_data
+  agency_data$tj <- rep(tj, each = t)
+  
+  # Create treatment period dummy
+  agency_data <- agency_data %>% 
+    mutate(treated = ifelse(period - tj >= 0, 1, 0))
+  
+  # Create post treatment recidivism probability
+  agency_data <- agency_data %>% 
+    mutate(pj_post = ifelse(treated == 1, pj - rho, pj))
+  
   # Make data
-  data <- generate_data_fast(agency_data, delta = 0)
+  data <- generate_data_fast(agency_data, delta = rho)
   
   # Unique id
-  data <- data %>% mutate(uid = paste(id, tj, sep = "_"))
+  data <- data %>% mutate(uid = paste(id, tj, agency, sep = "_"))
   
   # Callaway-Sant'Anna
   twfe_cs <- staggered::staggered_cs(df = data,
@@ -21,11 +92,11 @@ for (i in 1:M){
   
   # Roth-Sant'Anna
   twfe_rs <- staggered::staggered(df = data,
-                                         i = "uid", # Cross-sectional unit identifier
-                                         t = "period", # Time period column
-                                         g = "tj", # First period observation is treated,
-                                         y = "y", # Outcome variable
-                                         estimand = "simple")
+                                  i = "uid", # Cross-sectional unit identifier
+                                  t = "period", # Time period column
+                                  g = "tj", # First period observation is treated,
+                                  y = "y", # Outcome variable
+                                  estimand = "simple")
   
   # Sun-Abraham
   twfe_sa <- staggered::staggered_sa(df = data,
@@ -35,12 +106,9 @@ for (i in 1:M){
                                   y = "y", # Outcome variable
                                   estimand = "simple")
   
-  
-  # sunab <- sunab_reg_bin_pre_post$coeftable[2,1]
-  rthsnt <- twfe_rs[1] %>% as.numeric
-  calsnt <- twfe_cs[1] %>% as.numeric
-  sunab <- twfe_sa[1] %>% as.numeric
-  store[[i]] <- c(sunab, rthsnt, calsnt)
+  store[[i]] <- c(as.numeric(twfe_sa["estimate"]),
+                  as.numeric(twfe_rs["estimate"]),
+                  as.numeric(twfe_cs["estimate"]))
   
 }
 
@@ -51,10 +119,12 @@ store %>%
 store %>% 
   do.call(rbind, .) %>% 
   as.data.frame() %>% 
-  setNames(c("Sun-Abraham", "Roth-Sant'Anna", "Callaway-Sant'Anna")) %>% 
+  setNames(c("Sun-Abraham", 
+             "Roth-Sant'Anna", 
+             "Callaway-Sant'Anna")) %>% 
   pivot_longer(., cols = everything()) %>% 
   ggplot(data = ., aes(x = value, fill = name)) + 
   geom_density(alpha = 0.5) +
   theme_minimal() +
   labs(title = "Treatment Effects") + 
-  geom_vline(xintercept = 0.05)
+  geom_vline(xintercept = rho)
